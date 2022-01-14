6b9b4448d134008f4a429114f44f867b4cfacc00
Atique Akhtar <atiqueakhtar786@gmail.com>
2022-01-14 15:05:30 +0530

    added home and about views and flash

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "about", to: "about#index"
   root to: "main#index"
+  get "about-us", to: "about#index", as: :about
   # Defines the root path route ("/")
   # root "articles#index"
 end
